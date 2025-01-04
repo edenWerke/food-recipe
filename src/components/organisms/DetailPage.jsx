9f3ea1772a4774cd29603289141741ef53981090
edenWerke <edenw6292@gmail.com>
2025-01-05 00:05:46 +0300

    creating page

---
+++ src/components/organisms/DetailPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../molecules/Header'
+import GridCards2 from '../molecules/GridCards2'
+const DetailPage = () => {
+  return (
+    <div>
+<Header/>
+<GridCards2/>
+    </div>
+  )
+}
+
+export default DetailPage
