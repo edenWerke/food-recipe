aa19249d74d4b36a43c04e3a5fc95fe622c4c091
edenWerke <edenw6292@gmail.com>
2025-01-05 00:03:30 +0300

    changing th App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/molecules/Header";
+import DetailPage from "./components/organisms/DetailPage";
 import MainContent from "./components/organisms/MainContent";
 
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
+       {/* <Header /> */}
+      {/* <MainContent /> */}
+      <DetailPage/>
     </div>
   );
 }
